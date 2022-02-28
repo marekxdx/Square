c4e97fa70a149ad80aacc4b21ca3993b3dc9803f
Marek <Marek@MAREK-PC>
2022-02-28 10:17:12 +0100

    Dodaj pliki projektów.

---
+++ Aplikacja/Program.cs
@@ -0,0 +1,15 @@
+﻿using Aplikacja;
+
+Console.WriteLine("Welcome in store. You pay 20zł for square meter");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Write number of meters");
+int meters = int.Parse(Console.ReadLine()); 
+Square [] squares = new Square [meters];
+
+for (int i = 0; i < meters; i++)
+{
+    squares [i] = new Square (i + 1);
+    Console.WriteLine($"Lenght of site is {squares[i].Side} and square is {squares[i].Area}. The payments is {squares[i].Surface} zł");
+}
+
---
+++ Aplikacja/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja
+{
+    internal class Square
+    {
+
+        private int side;
+        private int area;
+        private int surface;
+        public Square(int side)
+        {
+            this.side = side;  
+            calcArea(); 
+            calcSurface();
+        }
+
+        private void calcArea()
+        {
+            area = side * side;
+        }
+        private void calcSurface()
+        {
+            surface = area * 20;
+        }
+     
+        public int Side { get { return side; } }    
+        
+        public int Area { get { return area; } }
+
+        public int Surface { get { return surface; } }
+
+         
+    }
+}
